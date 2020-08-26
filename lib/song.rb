81b0b69ba5332f31ec85533adf20289ca39ccf80
Richard Kurwitz <kurwitz3@aol.com>
2020-08-26 21:10:31 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,40 @@
+require 'pry'
+class Song 
+  attr_accessor :name,:artist,:genre
+  @@count = 0 
+   @@artists = []
+  @@genres = []
+def initialize(name,artist,genre)
+  @@genres << genre 
+  @@artists << artist 
+  @@count += 1 
+ @name= name 
+ @artist = artist 
+ @genre = genre 
+end              
+def self.artists
+@@artists
+end 
+def self.count
+ @@count 
+end
+def self.genres 
+ @@genres.uniq
+end 
+def self.genre_count
+  count = {}
+  num = 1 
+@@genres.each  do |music|
+  if music.include?(music) == true 
+  count[music] = num + 1 
+ binding.pry 
+ end
+end
+count 
+ 
+
+end 
+
+  
+
+end 
